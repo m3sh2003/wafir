c09bd2dea312ea54c9de37d308ee41719e021c01
Wafir Agent <wafir@agent.com>
2026-01-17 21:21:22 +0300

    Fix: Revert debug controller to restore build stability

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { HealthController } from './health.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -44,7 +43,7 @@ import { RulesModule } from './rules/rules.module';
     RulesModule,
 
   ],
-  controllers: [AppController, HealthController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
